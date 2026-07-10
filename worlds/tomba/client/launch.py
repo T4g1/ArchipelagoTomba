1b9c563d72a40557edb63dafc3d0bdc9db53ece1
Van Gysegem Thomas <thomasvangysegem@gmail.com>
2026-07-10 16:28:55 +0200

    [add] Basic APWorld and client for Tomba! with RetroArch connection

---
+++ worlds/tomba/client/launch.py
@@ -0,0 +1,21 @@
+import asyncio
+import colorama
+
+from CommonClient import get_base_parser, handle_url_arg
+
+from .. import constants
+
+
+def launch_tomba_client(*args) -> None:
+    from TombaClient import main
+
+    parser = get_base_parser(description=f"{constants.GAME} Client.")
+    parser.add_argument("--name", default=None, help="Slot Name to connect as.")
+    parser.add_argument("url", nargs="?", help="Archipelago connection url")
+
+    launch_args = handle_url_arg(parser.parse_args(args))
+
+    colorama.just_fix_windows_console()
+
+    asyncio.run(main(launch_args))
+    colorama.deinit()
---
+++ worlds/tomba/constants.py
@@ -0,0 +1,14 @@
+GAME = "Tomba!"
+
+BASE_ID = 0
+
+VILLAGE_OF_ALL_BEGINNINGS = "Village Of All Beginnings"
+
+VILLAGE_OF_ALL_BEGINNINGS_MAILBOX = "Village Of All Beginnings Mailbox"
+
+VILLAGE_OF_ALL_BEGINNINGS_FOG_DISSIPATED = "Village Of All Beginnings Fog Dissipated"
+
+CHARITY_WINGS = "Charity Wings"
+FURIOUS_TORNADO = "Furious Tornado"
+
+VICTORY = "Victory"
